49c2144df1efd5841ae7563ba5ab67fbea6bb751
Sébastien Gross <sebchezwamorg>
2013-02-11 00:55:01 +0100

    Fix message suppression.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- terminal-notify.el
+++ terminal-notify.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2012-08-08
-;; Last changed: 2012-08-10 16:47:48
+;; Last changed: 2013-02-11 00:54:34
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -35,10 +35,11 @@ needed."
   (when (eq (process-status proc) 'exit)
     (let ((status  (process-exit-status proc))
 	  (timeout (process-get proc :timeout))
+	  (cmd-buf (process-get proc :cmd-buf))
 	  (id (process-get proc :id)))
       (if (not (eq 0 status))
 	  (progn)
-	(kill-buffer (process-buffer proc))
+	(kill-buffer cmd-buf)
 	(when (and timeout id (> timeout 0))
 	  (run-at-time timeout nil
 		       'terminal-notifier-run-cmd
@@ -53,6 +54,7 @@ directly (see `terminal-notifier-notify') ."
 		   (format " Terminal-notifier %s" (random))))
 	 (proc (apply 'start-process (car cmd-line)
 		      cmd-buf (car cmd-line) (cdr cmd-line))))
+    (process-put proc :cmd-buf cmd-buf)
     (process-put proc :timeout timeout)
     (process-put proc :id id)
     (set-process-sentinel proc 'terminal-notifier-sentinel)))
@@ -66,7 +68,7 @@ If TIMEOUT is positive, the notification is removed from the
 notification-center after TIMEOUT seconds or 10 seconds if
 TIMEOUT is nil."
   (let ((timeout (or timeout 10))
-	(id (format "%s" (random))))
+	(id (format "%s" (abs (random)))))
     (terminal-notifier-run-cmd
      `("-message" ,message "-title" ,title
        "-activate" ,terminal-notifier-emacs-bundle-id
